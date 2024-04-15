9b611e84bbefa992e554936c79cf2145711d4fd9
Pasichnichenkoilya <sheredan2003@gmail.com>
2024-04-15 21:58:36 +0300

    Add all cards fetch to main page

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,23 +1,78 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from "react";
 
-import { Card } from 'primereact/card';
+import { Card } from "primereact/card";
 
-import '../componentscss/MainPage.css'
-import MovieCard from '../components/MovieCard';
+import "../componentscss/MainPage.css";
+import MovieCard from "../components/MovieCard";
+import axios from "axios";
+import { Media } from "../types";
+
+const fetchAllCards = async (page: number) => {
+  const response = await fetch(
+    `https://kinolib-backend-homer.fly.dev/parse/all/${page}`
+  );
+  return await response.json();
+};
 
 const MainPage = () => {
+  // const [value, setValue] = useState<[number, number]>([0, 10]);
+  const [page, setPage] = useState(1);
+  const [mediaCards, setMediaCards] = useState<Media[]>();
 
-  const [value, setValue] = useState<[number, number]>([0, 10]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const cards = await fetchAllCards(page);
+      console.log(cards);
+    };
+    fetchData();
+  }, [page]);
+
   return (
-    <div className='bg-red-500 w-full h-10rem flex flex-row'>
-      <MovieCard/>
-      <MovieCard/>
-      <MovieCard/>
-      <MovieCard/>
-      <MovieCard/>
-    </div>
-      
-  )
-}
+    <div className="bg-red-500 w-full h-10rem flex flex-row">
+      {/* <MovieCard />
+      <MovieCard />
+      <MovieCard />
+      <MovieCard />
+      <MovieCard /> */}
 
-export default MainPage
+      {mediaCards?.map((card) => (
+        <div>
+          <p>
+            <span>id:</span>
+            <span>{card.id}</span>
+          </p>
+          <p>
+            <span>title:</span>
+            <span>{card.title}</span>
+          </p>
+          <p>
+            <span>image:</span>
+            <span>{card.image}</span>
+          </p>
+          <p>
+            <span>year:</span>
+            <span>{card.year}</span>
+          </p>
+          <p>
+            <span>rating:</span>
+            <span>{card.rating}</span>
+          </p>
+          <p>
+            <span>genres:</span>
+            <span>{card.genres}</span>
+          </p>
+          <p>
+            <span>type:</span>
+            <span>{card.type}</span>
+          </p>
+          <p>
+            <span>lastEpisode:</span>
+            <span>{card.lastEpisode}</span>
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default MainPage;
---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+export type Media = {
+  id: string;
+  title: string;
+  image: string;
+  year: number | null;
+  rating: number | null;
+  genres: string[];
+  type: string;
+  lastEpisode: string | null;
+};
