845a1bbeefaa84541e6a3533f18d65b457a2a75e
What1sL0v33 <vbirilko@gmail.com>
2024-04-30 02:30:30 +0300

    Search base

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { IconField } from "primereact/iconfield";
 import { InputIcon } from "primereact/inputicon";
 import { InputText } from "primereact/inputtext";
@@ -6,16 +6,56 @@ import "../styles/Search.css";
 
 const Search = () => {
   const [value, setValue] = useState<string>("");
+  const [suggestions, setSuggestions] = useState<{ label: string; value: string; }[]>([]);
+  const [isListVisible, setIsListVisible] = useState<boolean>(false); // Состояние для отслеживания видимости списка
+
+  const staticSuggestions = [
+    { label: "Фильм 1", value: "Фильм 1" },
+    { label: "Фильм 2", value: "Фильм 2" },
+    { label: "Фильм 3", value: "Фильм 3" },
+    { label: "Фильм 4", value: "Фильм 4" },
+    { label: "Фильм 5", value: "Фильм 5" }
+  ];
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setValue(value);
+
+    // Фильтрация предложений на основе введенного значения
+    const filteredSuggestions = staticSuggestions.filter(suggestion =>
+      suggestion.label.toLowerCase().includes(value.toLowerCase())
+    );
+    setSuggestions(filteredSuggestions);
+
+    // Установка видимости списка в зависимости от наличия фильтрованных предложений
+    setIsListVisible(value.trim() !== '' && filteredSuggestions.length > 0);
+  };
+
   return (
-    <IconField iconPosition="left">
-      <InputIcon className="pi pi-search text-white"> </InputIcon>
-      <InputText
-        v-model="value1"
-        placeholder="Пошук &#8201; фільмів, серіалів, аніме, мультфільмів тощо..."
-        className="w-full border-round-3xl search_bg border-none placeholder-color h-3rem custom-input"
-        style={{ color: "white" }}
-      />
-    </IconField>
+    <div>
+      <IconField iconPosition="left">
+        <InputIcon className="pi pi-search text-white" />
+        <InputText
+          value={value}
+          placeholder="Пошук фільмів, серіалів, аніме, мультфільмів тощо..."
+          className="w-full border-round-3xl search_bg border-none placeholder-color h-3rem custom-input"
+          style={{ color: "white" }}
+          onChange={handleInputChange}
+        />
+      </IconField>
+      {/* Отображение предложений, только если список видим */}
+      {isListVisible && (
+        <div className="suggestion-list-wrapper">
+          <div className="suggestion-list">
+            {suggestions.map((suggestion, index) => (
+              <div key={index} className="suggestion-item">
+                {suggestion.label}
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
   );
 };
 
