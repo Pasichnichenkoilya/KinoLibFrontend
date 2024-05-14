d1351fe5db8cc00adfc03e10473085d4b3997bee
Pasichnichenkoilya <sheredan2003@gmail.com>
2024-05-14 13:06:32 +0300

    Add cards to page when searching

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,62 +1,57 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 import { IconField } from "primereact/iconfield";
 import { InputIcon } from "primereact/inputicon";
 import { InputText } from "primereact/inputtext";
 
-import { Media } from "../types";
+import useDebounce from "../hooks/useDebounce";
+import { Media, MediaResponse } from "../types";
+import { useCardsContext } from "../hooks/useCards";
 
 import "../styles/Search.css";
 
-const fetchSearch = async (search: string) => {
+async function fetchSearch(search: string): Promise<MediaResponse> {
   const response = await axios.get(
     `https://kinolib-backend-homer.fly.dev/parse/search/${search}`
   );
   return response.data;
-};
-
-const useDebounce = (value: string, delay: number) => {
-  const [debouncedValue, setDebouncedValue] = useState(value);
-
-  useEffect(() => {
-    const handler = setTimeout(() => {
-      setDebouncedValue(value);
-    }, delay);
-
-    return () => {
-      clearTimeout(handler);
-    };
-  }, [value, delay]);
-
-  return debouncedValue;
-};
+}
 
 const Search = () => {
+  const { cards, setCards, setCountOfPages } = useCardsContext();
+  const wrapperRef = useRef<HTMLDivElement>(null);
   const [value, setValue] = useState("");
-  const [suggestions, setSuggestions] = useState<Media[]>([]);
-  const [isListVisible, setIsListVisible] = useState(false);
   const [isFocused, setIsFocused] = useState(false);
-
   const debouncedSearchValue = useDebounce(value, 100);
 
+  const isListVisible = cards.length > 0 && isFocused;
+
   useEffect(() => {
-    if (debouncedSearchValue.trim()) {
-      fetchSearch(debouncedSearchValue).then((data) => {
-        if (data && Array.isArray(data.media)) {
-          setSuggestions(data.media);
-          setIsListVisible(true);
-        } else {
-          console.error("Unexpected data format:", data);
-          setSuggestions([]);
-          setIsListVisible(false);
-        }
-      });
-    } else {
-      setSuggestions([]);
-      setIsListVisible(false);
-    }
+    const clickHandler = (e: MouseEvent) => {
+      if (wrapperRef.current === null || e.target === null) return;
+      if (!wrapperRef.current.contains(e.target as Node)) {
+        handleClose();
+      }
+    };
+    window.addEventListener("click", clickHandler);
+
+    return () => {
+      window.removeEventListener("click", clickHandler);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!debouncedSearchValue.trim()) return;
+
+    fetchSearch(debouncedSearchValue)
+      .then(({ media, countOfPages }) => {
+        setCards(media);
+        setCountOfPages(countOfPages);
+      })
+      .catch((e) => console.log(e));
   }, [debouncedSearchValue]);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -64,51 +59,51 @@ const Search = () => {
     setValue(value);
   };
 
-  const handleInputFocus = () => {
+  const handleOpen = () => {
     setIsFocused(true);
     document.body.classList.add("no-scroll");
   };
 
-  const handleInputBlur = () => {
+  const handleClose = () => {
     setIsFocused(false);
     document.body.classList.remove("no-scroll");
   };
 
-  const clearInput = () => {
-    setValue("");
-    setIsListVisible(false);
-  };
-
   return (
-    <div className={`search-container ${isFocused ? "focused" : ""}`}>
-      <IconField iconPosition="left" className="input-container">
-        <InputIcon className="pi pi-search text-white" />
-        <InputText
-          value={value}
-          placeholder="Пошук фільмів, серіалів, аніме, мультфільмів тощо..."
-          className="w-full border-round-3xl search_bg border-none placeholder-color h-3rem custom-input"
-          style={{ color: "white" }}
-          onChange={handleInputChange}
-          onFocus={handleInputFocus}
-          onBlur={handleInputBlur}
-        />
-        {value && (
-          <span
-            className="pi pi-times-circle clear-icon"
-            onClick={clearInput}></span>
-        )}
-      </IconField>
-      {isListVisible && (
-        <div className="suggestion-list-wrapper">
-          <div className="suggestion-list">
-            {suggestions.map((suggestion: Media) => (
-              <div key={suggestion.id} className="suggestion-item">
-                {suggestion.title}
-              </div>
-            ))}
+    <div
+      onFocus={handleOpen}
+      className={`search-container ${isFocused ? "focused" : ""}`}>
+      <div ref={wrapperRef}>
+        <IconField iconPosition="left" className="input-container">
+          <InputIcon className="pi pi-search text-white" />
+          <InputText
+            value={value}
+            placeholder="Пошук фільмів, серіалів, аніме, мультфільмів тощо..."
+            className="w-full border-round-3xl search_bg border-none placeholder-color h-3rem custom-input"
+            style={{ color: "white" }}
+            onChange={handleInputChange}
+          />
+          {value ? (
+            <span
+              className="pi pi-times-circle clear-icon"
+              onClick={() => setValue("")}></span>
+          ) : null}
+        </IconField>
+        {isListVisible ? (
+          <div className="suggestion-list-wrapper">
+            <div className="suggestion-list">
+              {cards.map((suggestion: Media) => (
+                <div
+                  onClick={handleClose}
+                  key={suggestion.id}
+                  className="suggestion-item">
+                  <Link to={`details${suggestion.id}`}>{suggestion.title}</Link>
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-      )}
+        ) : null}
+      </div>
     </div>
   );
 };
